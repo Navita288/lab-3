345721e5155fd16d3001d5e6acea29c4813c25c4
visualstudio <visualstudio@122-PC35>
2019-04-17 09:31:05 -0400

    Delegate

--- lab 3/Program.cs
+++ lab 3/Program.cs
@@ -6,10 +6,40 @@ using System.Threading.Tasks;
 
 namespace lab_3
 {
-    class Program
     {
-        static void Main(string[] args)
+    public class DelegateExercises
+    {
+        public delegate int MyDelegate(int intValue);
+
+        public int Method1(int intMethod1)
         {
+            return intMethod1 * 2;
+        }
+
+        public int Method2(int intMethod2)
+        {
+            return intMethod2 * 10;
+        }
+
+        public void Method3()
+        {
+            MyDelegate myDelegate = new MyDelegate(Method1);
+            int result1 = myDelegate(10);
+            System.Console.WriteLine(result1);
+            myDelegate = new MyDelegate(Method2);
+            int result2 = myDelegate(10);
+            System.Console.WriteLine(result2);
+        }
+    }
+
+    public class Program
+    {
+        public static void Main()
+        {
+            DelegateExercises delegateExercises = new DelegateExercises();
+            delegateExercises.Method3();
+            Console.ReadLine();
+
         }
     }
 }
